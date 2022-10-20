5e33bea6cc293b1d88f4933c3f1dafa498547260
nikita.klyutkin@gmail.com <nikita.klyutkin@gmail.com>
2022-10-20 15:16:31 +0300

    Add project files.

---
+++ Class1.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson5String
+{
+    internal class Class1
+    {
+        public void MaxWordNum(string str)
+        {
+            string [] textsplit = str.Split('\n', ' ', '.');
+            int Max = 0;
+            int Index = 0;
+            for (int slovo = 0; slovo < textsplit.Length; slovo++)
+            {
+                int currmax = 0;
+                for (int simvol = 0; simvol < textsplit[slovo].Length; simvol++)
+                {
+                    if (char.IsNumber(textsplit[slovo][simvol])) // Где i это слово k смвол 
+                    {
+                        currmax++;
+                    }
+                }
+                if (currmax > Max)
+                {
+                    Max = currmax;
+                    Index = slovo;
+                }
+            }
+            Console.WriteLine("Наибольше цифр в слове " + textsplit[Index]);
+        }
+        public void MaxWord(string str)
+        {
+            string[] textsplit = str.Split('\n', ' ', '.');
+            string maxlength = "";
+            for (int i = 0; i < textsplit.Length; i++)
+                if (maxlength.Length < textsplit[i].Length)
+                    maxlength = textsplit[i];
+
+            int count = 0;
+            foreach (string x in textsplit)
+            {
+                if (x == maxlength)
+                    ++count;
+            }
+            Console.WriteLine("Самое длинное слово: {0}\nСколько раз было: {1}", maxlength, count);
+        }
+        public void ReplaceNumToWord(string str)
+        {
+            Console.WriteLine("Замена цифр на слова : ");
+            string[] arr = { " ноль ", " один ", " два ", " три ", " четыре ", " пять ", " шесть ", " семь ", " восемь ", " девять " };
+            string lines = str;
+            for (int i = 0; i < 10; i++)
+                lines = lines.Replace(i.ToString(), arr[i]); // От 0 до 10 (i) следовательно i= 0 берем из массива 0 элемент 
+            Console.WriteLine(lines);
+        }
+        public void WithOutSentence(string str)
+        {
+            string[] sentences = str.Split('.', '\n');
+            Console.WriteLine("Предложение которые не содержат запятые:");
+            foreach (string sentence in sentences)
+            {
+                if (!sentence.Contains(','))
+                Console.WriteLine(sentence.Trim());
+            }
+        }
+        public void WordSameLetter(string str)
+        {
+            Console.WriteLine("Слова которые начинаются и заканчиваются на одну и ту же букву:");
+            var words = str.Split(' ', ',', '!', '?', '.', '-', '\n');
+            foreach (var word in words)
+            {
+                if (word.Length > 1 && word[0] == word[word.Length - 1])
+                {
+                    Console.WriteLine(word);
+                }
+            }
+        }
+
+        public void SentenceExcQue(string str)
+        {
+            string[] textsplit2 = str.Split('.', '\n');
+            Console.WriteLine("Сначала вопросительные, потом предложения:");
+            foreach (string stri in textsplit2)
+            {
+                if (stri.Contains('?') == true)
+                {
+                    Console.WriteLine(stri.Trim());
+                }
+            }
+            foreach (string stri in textsplit2)
+            {
+                if (stri.Contains('!') == true)
+                {
+                    Console.WriteLine(stri.Trim());
+                }
+            }
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,61 @@
+﻿
+using Lesson5String;
+using System.ComponentModel.Design;
+using System.Text;
+using static System.Net.Mime.MediaTypeNames;
+
+Class1 class1 = new Class1();
+string str = File.ReadAllText(@"\lesson5.txt");
+StreamReader reader = new StreamReader(@"\menu.txt", Encoding.Default);
+string menuFile = reader.ReadToEnd();
+reader.Close();
+Console.WriteLine(menuFile);
+
+while (true)
+{
+    Console.Write("Введите операцию из меню:");
+    string? menu = Console.ReadLine();
+    int intValue;
+    int selectmenu;
+    if (int.TryParse(menu, out intValue))
+    {
+        selectmenu = intValue;
+    }
+    else
+    {
+        Console.WriteLine("Не верный ввод. Попробуйте снова");
+        continue;
+    }
+    if(selectmenu == 1)
+    {
+        class1.MaxWordNum(str);
+    }
+    else if(selectmenu == 2)
+    {
+        class1.MaxWord(str);
+    }
+    else if(selectmenu == 3)
+    {
+        class1.ReplaceNumToWord(str);
+    }
+    else if(selectmenu == 4)
+    {
+        class1.SentenceExcQue(str);
+    }
+    else if (selectmenu == 5)
+    {
+        class1.WithOutSentence(str);
+    }
+    else if (selectmenu == 6)
+    {
+        class1.WordSameLetter(str);
+    }
+    else
+    {
+        Console.WriteLine("Такой операции не существует");
+    }
+    Console.WriteLine("Нажми Enter");
+    Console.ReadKey();
+    Console.Clear();
+    Console.WriteLine(menuFile);
+}
